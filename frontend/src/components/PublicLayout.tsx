132d0852f4d0e2c7bb189e68e7bcdabc4452e1df
Aryeisor <miguelbmx845@gmail.com>
2026-07-11 15:21:28 -0500

    Mejora experiencia en detalle de eventos

--- frontend/src/components/PublicLayout.tsx
+++ frontend/src/components/PublicLayout.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import SiteHeader from './SiteHeader';
 import Footer from './Footer';
 
 export default function PublicLayout() {
+  const location = useLocation();
+  const isEventDetail = /^\/evento\/[^/]+$/.test(location.pathname);
+
   return (
     <div className="app">
-      <SiteHeader />
+      {!isEventDetail && <SiteHeader />}
       <main>
         <Outlet />
       </main>
